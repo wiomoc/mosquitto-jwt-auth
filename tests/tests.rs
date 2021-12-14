584354337e4670327d9ef51dbc878bc1dc95a55d
Christoph Walcher <christoph-wa@gmx.de>
2021-12-14 17:59:59 +0100

    jwks integration test

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,3 +1,7 @@
+#[cfg(target_os = "linux")]
+extern crate libc;
+
+use std::fs;
 use std::process::{Command, Stdio};
 use std::thread;
 use std::time::Duration;
@@ -39,6 +43,55 @@ fn test_valid() {
 
     assert!(child.try_wait().unwrap().is_none());
     child.kill().unwrap();
+}
+
+#[test]
+#[cfg(target_os = "linux")]
+fn test_valid_jwks() {
+    fs::copy("tests/jwks.json", "jwks_test.json").unwrap();
+    let mut child = Command::new("mosquitto")
+        .args(&["-c", "tests/mosquitto_valid_jwks.conf"])
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    thread::sleep(Duration::from_secs(3));
+
+    assert_eq!(Command::new("mosquitto_pub")
+                   .stderr(Stdio::piped())
+                   .args(&["-h", "localhost",
+                       "-p", "3884",
+                       "-u", "name",
+                       "-P", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InN5bSJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJwdWJsIjpbIi8xMjMiXX0.dfWZVx6GG4ykZSZJNHIuNboWEztsNr34tAbr9uNMiU4",
+                       "-t", "/123",
+                       "-m", "abc"])
+                   .output()
+                   .unwrap()
+                   .stderr[..], b"Connection error: Connection Refused: not authorised.\n"[..]);
+
+    fs::copy("tests/jwks_with_sym.json", "jwks_test.json").unwrap();
+
+    // reload mosquitto config
+    unsafe {
+        libc::kill(child.id() as i32, libc::SIGHUP);
+    }
+    thread::sleep(Duration::from_secs(3));
+
+    assert_eq!(Command::new("mosquitto_pub")
+                   .stderr(Stdio::piped())
+                   .args(&["-h", "localhost",
+                       "-p", "3884",
+                       "-u", "name",
+                       "-P", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InN5bSJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJwdWJsIjpbIi8xMjMiXX0.dfWZVx6GG4ykZSZJNHIuNboWEztsNr34tAbr9uNMiU4",
+                       "-t", "/123",
+                       "-m", "abc"])
+                   .output()
+                   .unwrap()
+                   .stderr[..], b""[..]);
+
+    assert!(child.try_wait().unwrap().is_none());
+    child.kill().unwrap();
+    fs::remove_file("jwks_test.json").unwrap();
 }
 
 #[test]
